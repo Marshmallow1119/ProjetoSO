98a1fa2c1247a0d53ddb71d5e20ec83a4934916d
Tomás Brás <tomasbras@ua.pt>
2023-10-27 11:54:24 +0100

    Realizei alterações de hylobytte para byte

--- projeto.sh
+++ projeto.sh
@@ -8,17 +8,28 @@
 input() {
 
     #zona de teste
-    echo "$@"
-    echo "${@: -1}"
-    file=${@: -1}
-    du --time $file
-    du $file | grep -oE '[0-9.]+'
-    #fim de zona
-
+    echo "$@"                                                                       #vai exibir todos os argumentos passados
+    echo "${@: -1}"     
+    dir=${@: -1}                                                                    #atribui o último argumento á variável file
+    du --time $dir                                                                  #printa em bytes e não em kilobytes
+    size_in_bytes=$(du -b $dir | grep -oE '[0-9.]+')                                #size_in_kilobytes=$(du -b "$file" | awk '{print $1}') #alternativa que usa awk(temos de ver qual funciona)
+                                                                                    #size_in_bytes=$((size_in_kilobytes * 1024))
+    oper=""
+    oper="${oper}${${@: -3} ${@: -2} ${@: -1}"
+    ext=${@: -2} 
+     
+                                                                                    #fim de zona                                
+                                                                                    
     while getopts ":n:r:a:d:s:l" flag; do
         case $flag in
             n)
+            
+            info_file=$(stat "$dir")                                                 # Use o comando stat para obter informações detalhadas sobre o arquivo
+            date=$(echo "$informacoes" | grep "Modify:" | awk '{print $2, $3}')      #data de modificação do resultado
+            printf "%-10s %-50s %-25s %-10s\n" "SIZE" "NAME" "Modify" "$oper"
+            find "$dir" -type f -name "$ext" -exec du -h {} \; | awk '{print $1, $2}'
                 ;;
+
             r)
                 ;;
             a)
